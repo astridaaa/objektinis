26f0cbb59f708d298c292859bce95f383c876ebe
Astrida Jablonskytė <astrida789@gmail.com>
2025-02-08 12:34:27 +0200

    Header file with struct

---
+++ programa.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip> 
+#include <vector>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::vector;
+using std::endl;
+
+struct Stud{
+    string vardas, pavarde;
+    int EGrez;
+    vector <int> NDrez;
+};
