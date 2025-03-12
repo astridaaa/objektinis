3bf78bc3ebb2bdd0c37ce9730156daab3f69bc4d
Astrida Jablonskytė <astrida789@gmail.com>
2025-03-12 12:32:27 +0200

    Functions added: sorting and reading from a file | 'Strategija1.cpp'

---
+++ Strategijos/Strategija1.cpp
@@ -0,0 +1,82 @@
+//includas is funkciju main respiratory su ../#
+//includas nauju funkciju, kurios su templates startegija.h
+//mainas grn
+
+//leidziant linkinti du files nes dabar visas 3 startegijas paleidziu
+
+#include "strategija.h"
+#include "../functions.h"
+//Bendro studentai konteinerio (vector, list ir deque tipų) skaidymas (rūšiavimas) į du naujus to paties tipo konteinerius: "vargšiukų" ir "kietiakų". 
+//Tokiu būdu tas pats studentas yra dvejuose konteineriuose: bendrame studentai ir viename iš suskaidytų (vargšiukai arba kietiakai). Nesunku pastebėti, 
+//kad tokia strategija yra neefektyvi užimamos atminties atžvilgiu (įsitikinkite tuo!), tačiau šiame žingsnyje svarbiausia yra patyrinėti, kaip 
+//programos veikimo sparta priklauso nuo konteinerio tipo?
+int main(){
+    double visasLaikas = 0.0;
+    int konteineriuTipas;
+    int rusiavimasPagal;
+    std::vector<Stud> studentai;
+    std::vector<Stud> pirmunai;
+    std::vector<Stud> nesimokantys;
+
+    cout << "Programa testuojama su:" << endl;
+    cout << "1 | vector tipo konteineriu" << endl;
+    cout << "2 | deque tipo konteineriu" << endl;
+    cout << "3 | list tipo konteineriu" << endl;
+    cin >> konteineriuTipas;
+    
+    cout << "Studentus rusiuoti pagal:" << endl;
+    cout << "1 | Vardus\n";
+    cout << "2 | Pavardes\n";
+    cout << "3 | Galutini bala\n";
+    cout << "4 | Duomenu nerusiuoti\n";
+    cin >> rusiavimasPagal;
+    
+    //string testavimoFile = "Tyrimo_files\\" + filePasirinkimas(); //ima is tyrimu aplankalo
+
+
+
+    switch (konteineriuTipas)
+    {
+    case 1:
+    {
+
+        for(int a = 1000; a <= 10000000; a *= 10){
+            string testavimoFile = "Tyrimo_files\\Studentai" + std::to_string(a) + ".txt";
+            /*std::vector<Stud> studentai;
+            std::vector<Stud> pirmunai;
+            std::vector<Stud> nesimokantys;*/
+            nuskaitymasFile(rusiavimasPagal,testavimoFile, visasLaikas, studentai);
+            studentuIsskirstymas(studentai, visasLaikas, pirmunai, nesimokantys);
+        }
+        
+        break;
+    }
+      
+    case 2:
+    {
+      break;  
+    }
+
+    case 3: {
+
+    }
+
+    default:
+        cout << "Pasirinkite testavimo konteineri!\n";
+    }
+    
+
+
+
+    //nuskaitymas
+
+
+   
+    //sudedu i konteienri pagal template
+    //padarau sorta
+    //sudedu i kitus du konteinerius: pirmunus ir nesimokancius
+
+
+    
+    
+}
---
+++ Strategijos/strategija.h
@@ -0,0 +1,145 @@
+
+#include "../main.h"
+#include "../functions.h"
+
+#include <list>
+#include <type_traits>
+#include <deque>
+using std::list;
+using std::deque;
+using std::is_same_v;
+
+
+
+
+template <typename konteineris> 
+void nuskaitymasFile(int duomenuRusiavimas, string filePavadinimas, double &visasLaikas, konteineris &studentai){
+    double laikas = 0.0;
+    std::vector<string> visaeil;
+    Stud studentas;
+    int pazymys;
+    string eilute;
+    string eilute1;
+    ifstream f;
+    int iteracijos = 3;
+    std::stringstream bufferis;
+    f.open(filePavadinimas);
+
+    for(int i = 0; i < iteracijos; i++){
+        studentai.clear();
+        bufferis.str("");
+        bufferis.clear();
+        auto start = std::chrono::high_resolution_clock::now();
+        bufferis << f.rdbuf();
+        f.close();
+        getline(bufferis, eilute1); //praskippina pirma eilute
+        while (bufferis)
+        {
+            if (!bufferis.eof())
+            {
+                getline(bufferis, eilute);
+                visaeil.push_back(eilute);
+            }
+            else
+                break;
+        }
+        for(string s : visaeil)
+        {
+            std::istringstream f(s);
+            f >> studentas.vardas >> studentas.pavarde;
+            studentas.nd.clear();
+            while (f >> pazymys)
+            {
+                studentas.nd.push_back(pazymys);
+            }
+            studentas.egzaminas = studentas.nd.back();
+            studentas.nd.pop_back();
+            studentas.BalasGalutinisVid = galutinis(studentas, 1);
+            studentai.push_back(studentas);
+        }
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> diff = end - start;
+        if(i == 1 || i == 2){
+            laikas += diff.count();
+        }
+    }
+    visasLaikas += laikas / 2;
+    cout << "Failo nuskaitymas vidutiniskai truko " << laikas/2 << "s" << endl;
+    studentai.shrink_to_fit();
+}
+
+template <typename konteineris>
+void studentuIsskirstymas(konteineris& studentai, double &visasLaikas, konteineris& pirmunai, konteineris& nesimokantys){
+    double laikasProgramos = 0.0;
+
+    for(int j = 0; j < 3; j++){
+        auto start1 = std::chrono::high_resolution_clock::now();
+        for (Stud j : studentai)
+        {
+            if (j.BalasGalutinisVid >= 5)
+            {
+                pirmunai.push_back(j);
+            }
+            else
+                nesimokantys.push_back(j);
+        }
+        auto end1 = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> diff1 = end1 - start1;
+        if(j != 0){
+            laikasProgramos += diff1.count();
+        }
+        
+
+    }
+visasLaikas += laikasProgramos/2;
+cout << "Studentu isskirstymas i dvi grupes vidutiniskai uztruko: " << laikasProgramos/2 << endl;
+}
+
+template <typename konteineris>
+void studentuRusiavimas(int rusiavimasPagal, konteineris& pirmunai, konteineris& nesimokantys, double &visasLaikas){
+
+    if constexpr (is_same_v<konteineris, list<typename konteineris::value_type>>)
+    {
+        for(int i = 0; i < 3; i++){
+            if (rusiavimasPagal == 1)
+        {66+
+            sort(nesimokantys.begin(), nesimokantys.end(), PalygintiVardas);
+            sort(pirmunai.begin(), pirmunai.end(), PalygintiVardas);
+        }
+        if (rusiavimasPagal == 2)
+        {
+            sort(nesimokantys.begin(), nesimokantys.end(), PalygintiPavardes);
+            sort(pirmunai.begin(), pirmunai.end(), PalygintiPavardes);
+        }
+        if (rusiavimasPagal == 3)
+        {
+            sort(nesimokantys.begin(), nesimokantys.end(), PalygintiKategorijas);
+            sort(pirmunai.begin(), pirmunai.end(), PalygintiKategorijas);
+        }
+        }
+        
+    }
+    else{
+        if (rusiavimasPagal == 1)
+        {
+            nesimokantys.sort(PalygintiVardas);
+            pirmunai.sort(PalygintiVardas);
+        }
+        if (rusiavimasPagal == 2)
+        {
+            nesimokantys.sort(PalygintiPavardes);
+            pirmunai.sort(PalygintiPavardes);
+        }
+        if (rusiavimasPagal == 3)
+        {
+            nesimokantys.sort(PalygintiKategorijas);
+            pirmunai.sort(PalygintiKategorijas);
+        }
+    }
+}
+
+/*    cout << "1 | Vardus\n";
+    cout << "2 | Pavardes\n";
+    cout << "3 | Galutini bala\n";
+    cout << "4 | Duomenu nerusiuoti\n";*/
+
